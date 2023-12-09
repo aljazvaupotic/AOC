b3a0054563c5e57f34f41cdbbe9da5911595b9a2
Aljaz Vaupotic <aljaz.vaupotic@preskok.si>
2023-12-09 07:31:29 +0100

    Day 9
    Recursion baby

---
+++ 2023/day9.py
@@ -0,0 +1,33 @@
+def calculate_differences(arr):
+    if all(diff == 0 for diff in arr):
+        arr.append(0)
+        return arr
+
+    differences = [arr[i + 1] - arr[i] for i in range(len(arr) - 1)]
+    result = arr.copy()
+    result.append(calculate_differences(differences)[-1] + arr[-1])
+    return result
+
+
+def calculate_differences2(arr):
+    if all(diff == 0 for diff in arr):
+        arr.insert(0, 0)
+        return arr
+
+    differences = [arr[i + 1] - arr[i] for i in range(len(arr) - 1)]
+    result = arr.copy()
+    result.insert(0, result[0] - calculate_differences2(differences)[0])
+    return result
+
+
+# f = open("demo9.txt")
+f = open("input9.txt")
+data = f.readlines()
+values = [(x.strip()).split(" ") for x in data]
+r = []
+f = []
+for line in values:
+    tmp = list(map(int, line))
+    r.append(calculate_differences(tmp)[-1])
+    f.append(calculate_differences2(tmp)[0])
+print(sum(r), sum(f))
